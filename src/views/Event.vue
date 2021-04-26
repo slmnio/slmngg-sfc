dc1b9bf3b77050f69acc0e8f55f16be10addc0fb
Solomon Cammack <solomon@solomon.eu.com>
2021-04-26 01:05:21 +0100

    Add team groups (from team.team_category) including custom position

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -3,12 +3,18 @@
         <ThingTop :thing="event" type="event"></ThingTop>
 
         <div class="container mt-3">
-            <ContentRow title="Teams" v-if="event.teams && event.teams.length">
+            <ContentRow :title="group.name" v-for="group in groupedTeams" v-bind:key="group.name">
+                <ContentThing :thing="team" type="team" :theme="team.theme" v-for="team in group.teams" v-bind:key="team.id" :show-logo="true"></ContentThing>
+            </ContentRow>
+            <ContentRow title="Teams" v-if="event.teams && event.teams.length && !groupedTeams">
                 <ContentThing :thing="team" type="team" :theme="team.theme" v-for="team in event.teams" v-bind:key="team.id" :show-logo="true"></ContentThing>
             </ContentRow>
             <ContentRow title="Staff" v-if="event.staff && event.staff.length">
                 <ContentThing :thing="staff" type="player" :theme="event.theme" v-for="staff in event.staff" v-bind:key="staff.id"></ContentThing>
             </ContentRow>
+<!--            <ContentRow :title="team.name" v-for="team in event.teams" v-bind:key="team.id">-->
+<!--                <ContentThing :thing="player" type="player" :theme="team.theme" v-for="player in team.players" v-bind:key="player.id"></ContentThing>-->
+<!--            </ContentRow>-->
         </div>
     </div>
 </template>
@@ -26,12 +32,55 @@ export default {
     components: {
         ThingTop, ContentThing, ContentRow
     },
+    metaInfo() {
+        return {
+            title: this.event.name,
+            meta: [
+                { name: "description", content: "test description" },
+                { name: "og:description", content: "test description" },
+                { name: "og:title", content: this.event.name }
+            ]
+        };
+    },
     computed: {
+        groupedTeams() {
+            if (!this.event?.teams.some(team => team.team_category)) return null;
+            const categories = [];
+            this.event.teams.forEach(team => {
+                let categoryName = team.team_category;
+                let categoryPosition = null;
+
+                if (!categoryName) {
+                    // default to other group
+                    categoryName = "-1;Other";
+                }
+
+                if (categoryName.includes(";")) {
+                    // custom ordering
+                    categoryName = categoryName.split(";");
+                    categoryPosition = parseInt(categoryName.shift());
+                    categoryName = categoryName.join(" ");
+                }
+
+                if (!categories.find(category => category.name === categoryName)) {
+                    categories.push({ name: categoryName, teams: [], position: categoryPosition });
+                }
+
+                categories.find(category => category.name === categoryName).teams.push(team);
+            });
+            return categories.sort((a, b) => {
+                if (a.position === b.position) {
+                    return a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1;
+                }
+                return a.position - b.position;
+            });
+        },
         event() {
             return ReactiveRoot(this.id, {
                 theme: ReactiveThing("theme"),
                 teams: ReactiveArray("teams", {
-                    theme: ReactiveThing("theme")
+                    theme: ReactiveThing("theme"),
+                    players: ReactiveArray("players")
                 }),
                 staff: ReactiveArray("staff")
             });

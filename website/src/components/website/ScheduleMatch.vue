4c1e707014d17e3d36ea4288b69cf7c6003efaad
Solomon Cammack <solomon@solomon.eu.com>
2021-05-27 22:31:21 +0100

    Fix some schedule stuff - fixes #12

--- website/src/components/website/ScheduleMatch.vue
+++ website/src/components/website/ScheduleMatch.vue
@@ -2,19 +2,25 @@
     <div class="match my-2">
         <div class="match-left match-details flex-center">FT{{ match.first_to }}</div>
 
-        <div v-for="(team, i) in match.teams"
+        <div v-for="(team, i) in teams"
              v-bind:key="team.id" :style="{order: i*2}"
              class="match-team flex-grow-1 d-flex align-items-center justify-content-end"
              v-bind:class="{'right': i === 1}">
 
-            <router-link :to="url('team', team)" class="team-name d-none d-lg-flex">{{ team.name }}</router-link>
+            <div v-if="team.dummy" class="team-name team-name--spacer d-none d-lg-flex">{{ team.text }}</div>
+            <router-link v-else-if="!team.dummy" :to="url('team', team)" class="team-name d-none d-lg-flex">{{ team.name }}</router-link>
+
+
             <router-link :to="url('team', team)" class="team-code d-lg-none">{{ team.code }}</router-link>
-            <ThemeLogo :theme="team.theme" border-width="4" class="team-logo" icon-padding="4"/>
+
+
+            <ThemeLogo v-if="team && team.has_theme" :theme="team.theme" border-width="4" class="team-logo" icon-padding="4"/>
+            <div class="team-logo team-logo--spacer" v-else></div>
         </div>
 
         <router-link :to="url('match', this.match)" class="match-center match-vs flex-center">vs</router-link>
         <div class="match-right match-time flex-center">
-            <ScheduleTime :time="match.start"/>
+            <ScheduleTime :time="match.start" :custom-text="match.custom_name"/>
         </div>
     </div>
 </template>
@@ -30,7 +36,37 @@ export default {
     methods: { url },
     props: ["match"],
     computed: {
+        scores() {
+            if (!this.match) return [null, null];
+            if (!this.match.teams || this.match.teams.length !== 2) return [null, null];
+            return [this.match.score_1, this.match.score_2];
+        },
+        teams() {
+            const dummy = { text: "TBD", dummy: true, id: null };
+            if (!this.match) return [{ ...dummy, _empty: true }, { ...dummy, _empty: true }];
 
+            const text = (this.match.placeholder_teams || "").trim().split("|").filter(t => t !== "");
+
+            if (!this.match.teams || this.match.teams.length === 0) {
+                if (text.length === 2) {
+                    return text.map(t => ({ ...dummy, text: t }));
+                } else if (text.length === 1) {
+                    // eslint-disable-next-line no-unreachable
+                    if (this.match.placeholder_right) return [dummy, { ...dummy, text: text[0] }];
+                    return [{ ...dummy, text: text[0] }, dummy];
+                } else if (text.length === 0) {
+                    // no text, just use TBDs
+                    return [dummy, dummy];
+                }
+            }
+            if (this.match.teams.length === 1) {
+                if (this.match.placeholder_right) return [this.match.teams[0], { ...dummy, text: text[0] }];
+                return [{ ...dummy, text: text[0] }, this.match.teams[0]];
+            }
+
+            if (this.match.teams.length === 2) return this.match.teams;
+            return [];
+        }
     }
 };
 </script>
@@ -45,8 +81,12 @@ export default {
     .match-center { order: 1; }
     .match-right { order: 3; }
 
+    .match-team {
+        text-align: right;
+    }
     .match-team.right {
         flex-direction: row-reverse;
+        text-align: left;
     }
 
     .match {
@@ -69,6 +109,7 @@ export default {
         padding: 0 .6em;
         font-size: 1.25em;
         color: white;
+        line-height: 1;
     }
     .match-time {
         text-align: right;
--- website/src/components/website/ScheduleTime.vue
+++ website/src/components/website/ScheduleTime.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="schedule-time">
+        <div v-if="customText" class="custom-text mt-1">{{ customText }}</div>
         <div class="top" v-if="top">{{ top }}</div>
         <div class="bottom" v-if="bottom">{{ bottom }}</div>
     </div>
@@ -12,7 +13,7 @@ import store from "@/thing-store";
 
 export default {
     name: "ScheduleTime",
-    props: ["time"],
+    props: ["time", "customText"],
     computed: {
         activeTimezone() {
             const stz = store.state.timezone;
@@ -28,14 +29,14 @@ export default {
         },
         top() {
             // console.log(informal.display(this._time));
-            return this._time.format("{date} {month-short}");
+            return this.time && this._time.format("{date} {month-short}");
         },
         bottom() {
             console.log(this.activeTimezone);
             const display = informal.display(this.activeTimezone);
             const abbrev = this._time.isDST() ? display.daylight.abbrev : display.standard.abbrev;
 
-            return this._time.time() + " " + abbrev;
+            return this.time && this._time.time() + " " + abbrev;
 
 
             // console.log(informal.display(this._time.format("timezone")));
@@ -48,5 +49,11 @@ export default {
 <style scoped>
     .top {
         font-weight: bold;
+    }
+    .custom-text {
+        font-size: 12px;
+        font-weight: bold;
+        text-transform: uppercase;
+        line-height: 1;
     }
 </style>

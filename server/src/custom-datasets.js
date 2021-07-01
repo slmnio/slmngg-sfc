1b3844ce3b20324e6b2977f65e8541b7624404cc
Solomon Cammack <solomon@solomon.eu.com>
2021-07-01 21:41:27 +0100

    Add custom table update hook to build a player list #17

--- server/src/custom-datasets.js
+++ server/src/custom-datasets.js
@@ -4,6 +4,7 @@
 
 function tableUpdated(tableName, Cache) {
     if (tableName === "Matches") matchUpdate(Cache);
+    if (tableName === "Players") playerList(Cache);
 }
 module.exports = tableUpdated;
 
@@ -12,4 +13,20 @@ async function matchUpdate(Cache) {
     if (!allMatches.ids) return;
     let liveMatches = (await Promise.all(allMatches.ids.map(id => (Cache.get(id.slice(3)))))).filter(match => match.live);
     Cache.set("special:live-matches", { matches: liveMatches.map(m => m.id) });
+}
+
+async function playerList(Cache) {
+    // TODO: one problem: cache won't show removals - this is a site-wide issue
+    let allPlayers = await Cache.get("Players");
+    if (!allPlayers.ids) return;
+    let players = (await Promise.all(allPlayers.ids.map(id => (Cache.get(id.slice(3))))));
+
+
+    players = players.map(p => ({
+        id: p.id,
+        name: p.name,
+        verified: !!p.verified
+    })).filter(p => p.name);
+
+    Cache.set("special:players", { players });
 }

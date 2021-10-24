3bcc429f127e62339ed1f435dae138e10dd0ff9f
Solomon Cammack <solomon@solomon.eu.com>
2021-10-24 20:17:48 +0100

    Change roster overlay to force override overlay--bg

--- website/src/components/broadcast/RosterOverlay.vue
+++ website/src/components/broadcast/RosterOverlay.vue
@@ -1,5 +1,5 @@
 <template>
-    <GenericOverlay class="roster-overlay" :title="title || 'Rosters'" :accent-color="accentColor" body-color="transparent">
+    <GenericOverlay class="roster-overlay" :title="title || 'Rosters'" :accent-color="accentColor" body-color="transparent !important">
         <div class="team" v-for="team in teams" v-bind:key="team.id">
             <div class="team-top flex-center" :style="themeColor(team)">
                 <div class="team-name flex-center">{{ team.name }}</div>

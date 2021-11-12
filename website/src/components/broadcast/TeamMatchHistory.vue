982682e634fd95569b8f24d799128be07e7bab92
Solomon Cammack <solomon@solomon.eu.com>
2021-11-12 07:48:15 +0000

    Expanded filter for team match history on history overlay

--- website/src/components/broadcast/TeamMatchHistory.vue
+++ website/src/components/broadcast/TeamMatchHistory.vue
@@ -18,7 +18,12 @@ export default {
         matches() {
             if (!this.team?.matches) return [];
             return (this.team.matches || [])
-                .filter(m => m.id === this.match.id ? ([m.score_1, m.score_2].includes(m.first_to)) : true)
+                .filter(m => {
+                    if (!m.event) return false;
+                    if (this.match.week && m.week && this.match.week < m.week) return false; // no future matches?
+                    if (m.id === this.match.id) return ([m.score_1, m.score_2].includes(m.first_to));
+                    return true;
+                })
                 .sort(sortMatches) // in date order
                 .reverse() // reverse date order
                 .slice(0, this.max);

022c266a08e808b8ec47805e2d3841b5f1852d72
Solomon Cammack <solomon@solomon.eu.com>
2021-11-22 21:40:42 +0000

    Always show cams on desk cams overlay

--- website/src/components/broadcast/DeskCamsOverlay.vue
+++ website/src/components/broadcast/DeskCamsOverlay.vue
@@ -1,6 +1,6 @@
 <template>
     <GenericOverlay class="desk-cams-overlay" :title="title || 'Player Cams'" :accent-color="accentColor">
-        <div class="team-cams flex-center w-100 flex-column" v-show="broadcast.show_cams">
+        <div class="team-cams flex-center w-100 flex-column">
             <TeamCamsGroup v-for="(team, ti) in teams" v-bind:key="team.id"
                            :team="team" :guests="guests[ti]" :params="params" :event="broadcast && broadcast.event" />
         </div>
@@ -37,13 +37,16 @@ export default {
                 return t.theme && !t.theme.__loading && t.theme.id;
             })) return [];
             if (this.match.teams.length !== 2) return [];
-            return this.match.teams;
+            const teams = this.match.teams;
+            if (this.match.flip_teams) return teams.reverse();
+            return teams;
         },
         guests() {
             const _teams = [
                 this.broadcast.team_1_cams || [],
                 this.broadcast.team_2_cams || []
             ];
+            if (this.match.flip_teams) return _teams.reverse();
             return _teams;
         },
         params() {

301d1cc339aa4debfd93d2ec074c63907c94d8a5
Solomon Cammack <solomon@solomon.eu.com>
2021-12-24 06:57:09 +0000

    Improve loading on player match pages by moving reactive code to a single component

--- website/src/views/sub-views/PlayerMatches.vue
+++ website/src/views/sub-views/PlayerMatches.vue
@@ -1,28 +1,23 @@
 <template>
     <div>
         <div class="container">
-          {{ matches.length }}
             <div class="role-group" v-for="rel in mainPlayerRelationships" v-bind:key="rel.meta.singular_name">
                 <h1>as {{ rel.meta.singular_name }} ({{ rel.items.length }})</h1>
                 <div class="row">
                     <Match class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3"
                            v-for="item in rel.items"
-                           v-bind:key="item.item"
-                           :id="item.item"
+                           v-bind:key="item.item.id"
+                           :hydrated-match="item.item"
                     />
                 </div>
             </div>
         </div>
     </div>
 </template>
-<!--
-                           v-for="match in matches"
-                           v-bind:key="match.id"
-                           :hydrated-match="match"
--->
+
 <script>
 import Match from "@/components/website/match/Match";
-import { ReactiveCacheArray } from "@/utils/reactive";
+import { ReactiveArray, ReactiveThing } from "@/utils/reactive";
 import { sortMatches } from "@/utils/sorts";
 
 export default {
@@ -32,17 +27,29 @@ export default {
         Match
     },
     computed: {
-        matches() {
-            return ReactiveCacheArray(this.relationshipMatches);
-        },
-        relationshipMatches() {
-            return Object.values(this.mainPlayerRelationships).map(rel => rel.items).flat().map(i => i.item);
+        relationships() {
+            if (!this.player?.player_relationships?.length) return [];
+            return ReactiveArray("player_relationships", {
+                event: ReactiveThing("event", {
+                    theme: ReactiveThing("theme")
+                }),
+                teams: ReactiveArray("teams", {
+                    theme: ReactiveThing("theme")
+                }),
+                matches: ReactiveArray("matches", {
+                    event: ReactiveThing("event", {
+                        theme: ReactiveThing("theme")
+                    }),
+                    teams: ReactiveArray("teams", {
+                        theme: ReactiveThing("theme")
+                    })
+                })
+            })(this.player);
         },
         mainPlayerRelationships() {
-            if (!this.player?.player_relationships) return {};
+            if (!this.relationships) return {};
             const groups = {};
-
-            this.player.player_relationships.forEach(rel => {
+            this.relationships.forEach(rel => {
                 if (!groups[rel.singular_name]) {
                     groups[rel.singular_name] = {
                         meta: {
@@ -53,19 +60,16 @@ export default {
                         items: []
                     };
                 }
-
                 groups[rel.singular_name].items = groups[rel.singular_name].items.concat([
                     ...(rel.matches ? rel.matches.map(e => ({ item: e, type: "match" })) : [])
                 ]);
             });
-
             Object.entries(groups).forEach(([key, val]) => {
                 if (val.items.length === 0) {
                     delete groups[key];
                 }
                 if (groups[key] && groups[key].matches) groups[key].matches = groups[key].matches.sort(sortMatches);
             });
-
             return groups;
         }
     }

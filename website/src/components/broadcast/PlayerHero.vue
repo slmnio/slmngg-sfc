64c1a896870bcc49f5a2d6f9abcd6d6c9d536990
Jordan Medrano <jordanmedrano@gmail.com>
2022-01-13 03:54:33 -0600

    create a generic component that displays a hero's name, role, and portrait

---
+++ website/src/components/broadcast/PlayerHero.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="hero-card d-flex flex-column flex-center" v-if="hero">
+    <div class="hero-name d-flex flex-row text-center font-weight-bold"><span v-if="roleIcon" class="icon" v-html="roleIcon"></span> {{heroName}}</div>
+    <div class="hero-role d-flex flex-row text-center">{{heroRole}}</div>
+    <div class="hero-portrait d-flex flex-row flex-center"><img :src="heroPortraitURL"/></div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: "PlayerHero",
+    props: ["hero", "british"],
+    computed: {
+        heroName() {
+            if (!this.hero?.name) return null;
+            return this.hero.name;
+        },
+        heroRole() {
+            if (!this.hero?.name) return null;
+            return this.hero.role;
+        },
+        roleIcon() {
+            switch (this.heroRole) {
+            case "Tank":
+                return "<i class=\"fas fa-shield-alt\"></i>";
+            case "Support":
+                return "<i class=\"fas fa-plus-circle\"></i>";
+            case "DPS":
+                return "<i class=\"fas fa-crosshairs\"></i>";
+            }
+            return null;
+        },
+        heroPortraitURL() {
+            if (!this.hero?.main_image) return null;
+            return this.hero?.main_image[0].url;
+        }
+    }
+
+
+};
+</script>
+
+<style scoped>
+  .hero-card {
+    margin: 10px;
+  }
+  .hero-name {
+    font-size: 3em;
+  }
+  .hero-role {
+    font-size: 1.2em;
+  }
+  .icon {
+    margin-right: 5px;
+  }
+  .hero-portrait {
+    height: auto;
+    width: 300px;
+  }
+
+</style>

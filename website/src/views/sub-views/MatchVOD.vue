2c34f31aa432eba99a61923dd66cc1999dd6a91a
Solomon Cammack <solomon@solomon.eu.com>
2022-01-15 06:20:08 +0000

    Show 2nd VOD if set - closes #94

--- website/src/views/sub-views/MatchVOD.vue
+++ website/src/views/sub-views/MatchVOD.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="match-vod">
-        <EmbeddedVideo :src="match.vod" v-if="match && match.vod" />
+        <EmbeddedVideo :src="vodLink" v-if="vodLink" />
+
+        <div class="vod-2-holder d-flex justify-content-end w-100 mt-2" v-if="match.vod_2">
+            <div class="btn btn-sm text-light">This match has multiple VODs:</div>
+            <div class="btn btn-dark btn-sm" @click="useVOD2 = true" v-if="!useVOD2">Show part 2</div>
+            <div class="btn btn-dark btn-sm" @click="useVOD2 = false" v-if="useVOD2">Show part 1</div>
+        </div>
+
         <div class="embed embed-responsive embed-responsive-16by9" v-if="showNoVOD">
             <div class="no-embed-text flex-center">No VOD available for this match</div>
         </div>
@@ -19,6 +26,9 @@ export default {
     name: "MatchVOD",
     props: ["match"],
     components: { EmbeddedVideo, MapDisplay },
+    data: () => ({
+        useVOD2: false
+    }),
     computed: {
         showNoVOD() {
             console.log("_match", this.match, !!this.match);
@@ -26,6 +36,10 @@ export default {
             if (this.match.__loading || !this.match.id) return false;
             if (this.match && !this.match.vod) return true;
             return false;
+        },
+        vodLink() {
+            if (this.useVOD2) return this.match?.vod_2;
+            return this.match?.vod;
         },
         theme() {
             return this.match?.event?.theme;

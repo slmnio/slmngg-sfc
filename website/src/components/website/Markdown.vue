a95822a137841a46fde72dfb626f0b52784e7d48
Solomon Cammack <solomon@solomon.eu.com>
2022-01-19 21:15:33 +0000

    Add a clips feature to news articles

--- website/src/components/website/Markdown.vue
+++ website/src/components/website/Markdown.vue
@@ -10,17 +10,71 @@ const marked = require("marked");
 //     console.log("[image]", href, title, text);
 // };
 
-function getURL(html) {
-    const r = new RegExp("src=\"([^\"]*)\"");
+function getAttribute(attribute, html) {
+    const r = new RegExp(`${attribute}="([^"]*)"`);
     const results = html.match(r);
-    console.log(html, results);
+    // console.log(html, results);
     if (results && results[1]) return results[1];
     return null;
 }
 
+function getURL(html) {
+    return getAttribute("src", html);
+}
+
+function getClipEmbed(url) {
+    // console.log({ url });
+    // if (url.startsWith("<") && url.endsWith(">")) url = url.slice(1, -1);
+    // console.log({ url });
+    const clipURL = new URL(url);
+    console.log(clipURL);
+
+    if (clipURL.href.startsWith("https://clips.twitch.tv/embed")) {
+        // already embedd link aa
+        clipURL.searchParams.set("parent", window.location.hostname);
+        return clipURL.href;
+    }
+
+    let clipSlug;
+    if (clipURL.hostname === "clips.twitch.tv") {
+        // clips.twitch.tv/SLUG
+        const end = clipURL.pathname.indexOf("?") > 0 ? clipURL.pathname.indexOf("?") : clipURL.pathname.length;
+        console.log({ end, clipURL });
+        clipSlug = clipURL.pathname.slice(1, end);
+    }
+    if (clipURL.hostname === "twitch.tv") {
+        // twitch.tv/CHANNEL/clip/SLUG
+        clipSlug = clipURL.pathname.split("/")[3];
+
+        const end = clipSlug.indexOf("?") > 0 ? clipSlug.indexOf("?") : clipSlug.length;
+
+        console.log({ end, clipSlug });
+        clipSlug = clipSlug.slice(0, end);
+    }
+    console.log("slug!", clipSlug);
+
+    if (clipSlug) {
+        return `https://clips.twitch.tv/embed?clip=${clipSlug}&parent=${window.location.hostname}`;
+    }
+
+    return "";
+}
+
 const renderer = {
     html(html) {
-        console.log(html);
+        html = html.replaceAll("&parenthere", `&parent=${window.location.hostname}`);
+        console.log("html", html);
+
+        if (html.trim().startsWith("<clip")) {
+            try {
+                console.log("clip html", html);
+                const url = getClipEmbed(getURL(html));
+                const caption = getAttribute("caption", html);
+                if (url) return `<div class="clip-holder"><div class="clip embed embed-responsive embed-responsive-16by9"><iframe src="${url}"></iframe></div>${caption ? `<div class="clip-caption">${caption}</div>` : ""}</div>`;
+            } catch (e) { console.error("Clip rendering error", e); }
+            return "<div class=\"clip clip-error\">An error prevented this clip from rendering</div>";
+        }
+
         if (html.trim().startsWith("<img")) {
             console.log(html);
             const url = getURL(html);
@@ -60,5 +114,17 @@ export default {
     }
     .markdown >>> a {
         color: var(--theme-active, #66d9ff);
+    }
+    .markdown >>> .clip-holder {
+        margin-bottom: 1em;
+    }
+    .markdown >>> .clip.clip-error {
+        background-color: rgba(0,0,0,0.2);
+        text-align: center;
+        padding: 10px 20px;
+    }
+    .markdown >>> .clip-caption {
+        background-color: rgba(0,0,0,0.2);
+        padding: 10px 15px;
     }
 </style>

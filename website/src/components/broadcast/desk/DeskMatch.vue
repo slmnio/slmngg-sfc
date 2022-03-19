54465f1270297230257eae75aabe0deb40856fda
Solomon Cammack <solomon@solomon.eu.com>
2022-03-19 06:50:59 +0000

    Fix some desk match errors

--- website/src/components/broadcast/desk/DeskMatch.vue
+++ website/src/components/broadcast/desk/DeskMatch.vue
@@ -1,5 +1,5 @@
 <template>
-        <div class="desk-match">
+        <div class="desk-match" v-if="match">
             <div class="teams d-flex" v-if="!match.special_event">
                 <DeskTeam class="team" v-for="(team, i) in match.teams" v-bind:key="team.id" :team="team" :style="{order: i * 2}" />
 

f1f3905a437879b06a078edb3a6a1bff2aa23182
Solomon Cammack <solomon@solomon.eu.com>
2022-03-25 16:59:30 +0000

    Alter styling so it can use fallbacks

--- website/src/utils/theme-styles.js
+++ website/src/utils/theme-styles.js
@@ -1,10 +1,14 @@
 export function logoBackground(theme) {
     if (!theme) return {};
-    return {
+    const style = {
         backgroundColor: theme.color_logo_background ? theme.color_logo_background : theme.color_theme,
         color: theme.color_logo_background ? theme.color_text_on_logo_background : theme.color_text_on_theme,
         borderColor: theme.color_logo_accent
     };
+    if (!style.backgroundColor) delete style.backgroundColor;
+    if (!style.color) delete style.color;
+    if (!style.borderColor) delete style.borderColor;
+    return style;
 }
 export function logoBackground1(item) {
     if (!item || !item.theme) return {};
@@ -13,11 +17,15 @@ export function logoBackground1(item) {
 
 export function themeBackground(theme) {
     if (!theme) return {};
-    return {
+    const style = {
         backgroundColor: theme.color_theme || theme.color_logo_background,
         color: theme.color_text_on_theme || theme.color_alt || theme.color_text_on_logo_background,
         borderColor: theme.color_accent
     };
+    if (!style.backgroundColor) delete style.backgroundColor;
+    if (!style.color) delete style.color;
+    if (!style.borderColor) delete style.borderColor;
+    return style;
 }
 export function themeBackground1(item) {
     if (!item || !item.theme) return {};

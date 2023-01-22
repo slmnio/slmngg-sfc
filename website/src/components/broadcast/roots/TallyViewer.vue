f0cf02b9bb4622a444d3c546e646c3edd3be3cc2
Solomon Cammack <solomon@solomon.eu.com>
2023-01-22 02:23:35 +0000

    Improve tally viewer data

--- website/src/components/broadcast/roots/TallyViewer.vue
+++ website/src/components/broadcast/roots/TallyViewer.vue
@@ -11,12 +11,23 @@
                     <span v-else><i class="fas fa-exclamation-circle"></i> Not assigned </span>
                     <span>&middot; {{ client.name }}</span>
                 </div>
-                <div class="prod-info flex-center" v-if="producer && showProducerInfo">
-                    <div class="prod-name">Producer: {{ producer.name }}</div>
-                    <div class="prod-scenes">
-                        <div class="prod-program">{{ producerProgramScene }}</div>
-                        <div class="prod-preview">{{ producerPreviewScene }}</div>
-                    </div>
+            </div>
+        </div>
+        <div class="tally-overlay">
+            <div class="team-info" v-if="teams">
+                <div class="team" v-for="(team, i) in teams" :key="team.id" :style="{order: (2 * i) + 1}">
+                    <div class="team-name">{{ team.name }}</div>
+                    <ThemeLogo class="team-logo" :theme="team.theme" border-width=".3em" icon-padding="1em"  />
+                    <div class="team-score">{{ scores[i] }}</div>
+                </div>
+                <div class="first-to">{{ liveMatch.first_to ? `FT${liveMatch.first_to}` : 'vs' }}</div>
+            </div>
+            <div class="spacer h-100"></div>
+            <div class="prod-info" v-if="producer">
+                <div class="prod-name flex-center">Producer: {{ producer.name }}</div>
+                <div class="prod-scenes">
+                    <div class="prod-preview">{{ producerPreviewScene }}</div>
+                    <div class="prod-program">{{ producerProgramScene }}</div>
                 </div>
             </div>
         </div>
@@ -24,10 +35,12 @@
 </template>
 
 <script>
-import { ReactiveArray, ReactiveRoot } from "@/utils/reactive";
+import { ReactiveArray, ReactiveRoot, ReactiveThing } from "@/utils/reactive";
+import ThemeLogo from "@/components/website/ThemeLogo.vue";
 
 export default {
     name: "TallyViewer",
+    components: { ThemeLogo },
     props: ["client", "scene"],
     sockets: {
         tally_change({ state, number }) {
@@ -58,8 +71,23 @@ export default {
             const matchID = this.client?.broadcast?.live_match?.[0];
             if (!matchID) return null;
             return ReactiveRoot(matchID, {
-                player_relationships: ReactiveArray("player_relationships")
+                player_relationships: ReactiveArray("player_relationships"),
+                teams: ReactiveArray("teams", {
+                    theme: ReactiveThing("theme")
+                })
             });
+        },
+        teams() {
+            if (!this.liveMatch?.teams?.length) return [];
+            const teams = this.liveMatch?.teams;
+            if (this.liveMatch.flip_teams) return teams.reverse();
+            return teams;
+        },
+        scores() {
+            if (!this.liveMatch) return [null, null];
+            const scores = [this.liveMatch.score_1, this.liveMatch.score_2];
+            if (this.liveMatch.flip_teams) return scores.reverse();
+            return scores;
         },
         selfObserverNumber() {
             return (this.liveMatch?.player_relationships || []).filter(rel => rel.singular_name === "Observer").findIndex(rel => rel.player?.[0] === this.client?.staff?.[0]) + 1;
@@ -143,31 +171,70 @@ export default {
   background-color: #ff0000;
 }
 
-.prod-info {
-    flex-direction: column;
+.prod-info, .team-info {
+    display: flex;
     text-align: center;
     font-size: 0.4em;
     background-color: rgba(32,32,32,0.5);
     color: white;
     padding: .5em 1em;
-    margin-top: 2em;
+    width: 100%;
+}
+
+.team-info {
+    padding: 0;
+}
+.team-name {
+    padding: .5em 1em;
+}
+.team-score {
+    padding: .2em 1em;
+}
+
+.team {
+    display: flex;
+    width: 50%;
+    justify-content: center;
+    align-items: center;
+}
+.team-info .team:nth-child(2) {
+    flex-direction: row-reverse;
+}
+.team-name {
+    flex-grow: 1;
+    text-align: center;
+    line-height: 1.2;
+}
+.team-logo {
+    height: 100%;
+    width: 3em;
 }
 
 .prod-name {
     font-weight: bold;
-    margin-bottom: .2em;
+    padding: 0.1em .5em;
 }
 .prod-scenes {
     display: flex;
-    flex-direction: column;
+    justify-content: center;
+    align-items: center;
     font-size: 0.8em;
-    width: 100%;
+    flex-grow: 1;
+    margin-left: 1em;
+    font-weight: bold;
 }
 .prod-scenes div {
     border: 1px solid rgba(255,255,255,0.5);
-    padding: 0.1em .5em;
-    margin: 0.1em;
+    padding: 0.5em .25em;
+    margin: 0 0.25em;
     background-color: black;
+    width: 100%;
+    line-height: 1.2;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
 }
 
 .prod-scenes .prod-preview {
@@ -181,4 +248,26 @@ export default {
     border-radius: .1em;
 }
 
+.tally-overlay {
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.first-to {
+    order: 2;
+    padding: .5em .25em;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-weight: bold;
+}
 </style>

618032a1ea5500529215ddaa8f2cd988830196c7
Solomon Cammack <solomon@solomon.eu.com>
2023-01-22 05:11:31 +0000

    Update broadcast switcher to split active and inactive matches

--- website/src/components/website/dashboard/BroadcastSwitcher.vue
+++ website/src/components/website/dashboard/BroadcastSwitcher.vue
@@ -6,8 +6,18 @@
 
         <b-modal ref="broadcast-switcher" id="broadcast-switcher" title="Broadcast Switcher" hide-footer>
             <div class="broadcasts flex-center flex-column">
-                <BroadcastDisplay class="broadcast" :disabled="bi === 0 || setting" v-for="(broadcast, bi) in broadcasts" :broadcast="broadcast" :key="broadcast.id" :set-method="switchBroadcast" />
+                <BroadcastDisplay class="broadcast" :disabled="bi === 0 || setting" v-for="(broadcast, bi) in broadcastGroups.active" :broadcast="broadcast" :key="broadcast.id" :set-method="switchBroadcast" />
+                <div class="broadcasts-text inactive" v-if="broadcastGroups.inactive.length">Inactive broadcasts</div>
+                <BroadcastDisplay class="broadcast inactive" :disabled="bi === 0 || setting" v-for="(broadcast, bi) in broadcastGroups.inactive" :broadcast="broadcast" :key="broadcast.id" :set-method="switchBroadcast" />
+
             </div>
+
+            <template v-slot:modal-footer>
+                <div class="w-100 flex-center text-center">
+                    Changing your broadcast will completely change your show's graphics.<br>
+                    Make sure that you are not streaming and ready for these graphics to change.
+                </div>
+            </template>
         </b-modal>
     </div>
 </template>
@@ -30,6 +40,22 @@ export default {
     computed: {
         activeBroadcast() {
             return this.broadcasts?.[0];
+        },
+        broadcastGroups() {
+            const groups = {
+                active: [],
+                inactive: []
+            };
+
+            (this.broadcasts || []).forEach(broadcast => {
+                if (broadcast?.active) {
+                    groups.active.push(broadcast);
+                } else {
+                    groups.inactive.push(broadcast);
+                }
+            });
+
+            return groups;
         }
     },
     methods: {
@@ -61,5 +87,15 @@ export default {
     .broadcasts .broadcast {
         margin: 0.25em 0;
         cursor: pointer;
+    }
+
+    .broadcast.inactive {
+        opacity: 0.8;
+        font-size: 12px;
+        width: 18.75em;
+    }
+
+    .broadcasts-text.inactive {
+        margin: .5em 0;
     }
 </style>

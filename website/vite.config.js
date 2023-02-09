e3cb4d47e6fb4743006a0c5e531a570be490ec38
Tobias Messner <tobias.d.messner@gmail.com>
2023-02-09 16:02:51 +0100

    Replace jimp with image-js to fix issues with fs

--- website/vite.config.js
+++ website/vite.config.js
@@ -11,8 +11,7 @@ export default defineConfig({
     plugins: [vue()],
     resolve: {
         alias: {
-            "@": fileURLToPath(new URL("./src", import.meta.url)),
-            fs: require.resolve("rollup-plugin-node-builtins")
+            "@": fileURLToPath(new URL("./src", import.meta.url))
         },
         extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"]
     }

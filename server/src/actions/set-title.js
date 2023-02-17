9044e22c47ea0bb64beabe3d2024311bf10fcbb4
Solomon Cammack <solomon@solomon.eu.com>
2023-02-17 23:18:41 +0000

    Update set-title action to have more format options and stop showing "undefined" when it breaks

--- server/src/actions/set-title.js
+++ server/src/actions/set-title.js
@@ -25,13 +25,18 @@ module.exports = {
 
         const formatOptions = {
             "event": event.name,
+            "event_name": event.name,
             "event_long": event.name,
             "event_short": event.short,
+
             "team_1_code": team1.code,
             "team_1_name": team1.name,
             "team_2_code": team2.code,
             "team_2_name": team2.name,
+
+            "match_custom_name": match.custom_name,
             "match_sub_event": match.sub_event,
+            "match_group": match.match_group,
             "match_round": match.round,
             "match_number": match.match_number,
         };
@@ -39,8 +44,10 @@ module.exports = {
         let newTitle = broadcast.title_format;
 
         Object.entries(formatOptions).forEach(([key, val]) => {
-            newTitle = newTitle.replace(`{${key}}`, val);
+            newTitle = newTitle.replace(`{${key}}`, val || "");
         });
+
+        newTitle = newTitle.trim();
 
         const gameMap = {
             "Overwatch": "Overwatch 2",

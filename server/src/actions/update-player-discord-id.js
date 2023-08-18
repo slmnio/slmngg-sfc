aed6a161db03ea204e60a18938ceec864f389b6d
MrLotU <j.koopman@jarict.nl>
2023-08-19 01:21:45 +0200

    So I think this will now make it so that there is maybe a modal that will pop up asking for confirmation or doing a dropdown. But who really knows

--- server/src/actions/update-player-discord-id.js
+++ server/src/actions/update-player-discord-id.js
@@ -7,14 +7,15 @@ const Cache = require("../cache");
 
 module.exports = {
     key: "update-player-discord-id",
-    requiredParams: ["discordData"],
+    requiredParams: ["discordData", "slmnggId"],
     auth: ["user"],
     /***
      * @param {User} discordData
+     * @param {string} slmnggId
      * @param {UserData} user
      * @returns {Promise<string>}
      */
-    async handler({ discordData }, { user }) {
+    async handler({ discordData, slmnggId }, { user }) {
         if (!canUpdateUserDetails(user)) {
             throw {
                 errorMessage: "You don't have permission update user details",
@@ -22,42 +23,7 @@ module.exports = {
             };
         }
 
-        let players = await Promise.all((await Cache.get("Players")).ids.map(id => (Cache.get(id.slice(3)))));
-
-        if (await Cache.auth.getPlayer(discordData.id)) {
-            throw {
-                errorMessage: "This user is already linked to a SLMN.GG profile",
-                errorCode: 400
-            };
-        }
-
-        const isNewUsername = discordData.discriminator === "0";
-
-        let searchValues = [discordData.username, discordData.globalName, !isNewUsername && `${discordData.username}#${discordData.discriminator}`]
-            .filter(Boolean)
-            .map((value) => value.toLocaleLowerCase());
-
-        let potentials = players.filter((player) => {
-            if (player.discord_id) return false;
-
-            const discordSimple = player.discord_tag?.split("#")[0].toLocaleLowerCase();
-
-            return searchValues.includes(discordSimple) || searchValues.includes(player.discord_tag?.toLocaleLowerCase()) || searchValues.includes(player.name?.toLocaleLowerCase());
-        });
-
-        if (potentials.length === 0) {
-            throw {
-                errorMessage: "Unable to find SLMN.GG user based on discord details",
-                errorCode: 404
-            };
-        } else if (potentials.length > 1) {
-            throw {
-                errorMessage: "Found more than 1 SLMN.GG player based on discord details",
-                errorCode: 500
-            };
-        }
-
-        const targetUser = potentials[0];
+        const targetUser = Cache.get(slmnggId);
 
         log("[profile]", user.airtable.name, user.airtable.id, "is setting discord id for", targetUser.name, targetUser.id, "to", discordData.id);
         let response = await this.helpers.updateRecord("Players", targetUser, {
--- server/src/discord/commands/admin/set-user-id.js
+++ server/src/discord/commands/admin/set-user-id.js
@@ -1,7 +1,14 @@
 const {
     ContextMenuCommandBuilder,
     userMention,
-    ApplicationCommandType
+    ApplicationCommandType,
+    ActionRowBuilder,
+    StringSelectMenuBuilder,
+    StringSelectMenuOptionBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    StringSelectMenuInteraction,
+    ButtonInteraction
 } = require("discord.js");
 const Cache = require("../../../cache.js");
 
@@ -28,18 +35,65 @@ module.exports = {
             return interaction.followUp({ ephemeral, content: "No action handlers can process your request." });
         }
 
-        await internalManager.runAction("update-player-discord-id", { discordData: interaction.targetUser }, token)
-            .then(async slmnggUser => {
-                await interaction.followUp({ ephemeral, content: `Linked discord user ${userMention(interaction.targetUser.id)} to SLMN.GG user ${slmnggUser}` });
-            })
-            .catch(({
-                errorCode,
-                errorMessage
-            }) => {
-                console.error({ errorCode, errorMessage });
-                interaction.followUp({
-                    ephemeral, content: `Action failed: ${errorMessage}`
-                });
+        const potentials = await internalManager.runAction("match-discord-slmngg-player", { discordData: interaction.targetUser }, token);
+        const rows = [];
+        var selectedUser = potentials[0].id;
+        var content = `Discord user ${userMention(interaction.targetUser.id)} will be linked to user ${selectedUser.name}`;
+        if (potentials.length !== 1) {
+            const options = potentials.map((p) => {
+                return new StringSelectMenuOptionBuilder()
+                    .setLabel(p.name)
+                    .setDescription(p.name)
+                    .setValue(p.id);
             });
+            const dropdown = new StringSelectMenuBuilder()
+                .setCustomId("slmngg-player")
+                .setPlaceholder("Select SLMNGG player")
+                .addOptions(options);
+            rows.push(new ActionRowBuilder().addComponents(dropdown));
+            content = "Please select the SLMN.GG user to link this account to.";
+            selectedUser = null;
+        }
+        const confirmButton = new ButtonBuilder()
+            .setCustomId("confirm")
+            .setLabel("Confirm")
+            .setStyle(ButtonStyle.Success);
+
+        const cancelButton = new ButtonBuilder()
+            .setCustomId("cancel")
+            .setDescription("Cancel")
+            .setStyle(ButtonStyle.Secondary);
+
+        rows.push(new ActionRowBuilder().addComponents(cancelButton, confirmButton));
+
+        const response = await interaction.followUp({ ephemeral, content, rows });
+
+        const collector = await response.createMessageComponentCollector({ time: 3_600_000 });
+
+
+        collector.on("collect", async i => {
+            if (typeof (i) === StringSelectMenuInteraction) {
+                selectedUser = i.values[0];
+            } else if (typeof (i) === ButtonInteraction) {
+                if (i.customId === "cancel") {
+                    await interaction.followUp({ ephemeral, content: "Cancelled action." });
+                } else if (i.customId === "confirm" && selectedUser !== null) {
+                    await internalManager.runAction("update-player-discord-id", { discordData: interaction.targetUser, slmnggId: selectedUser }, token)
+                        .then(async slmnggUser => {
+                            await interaction.followUp({ ephemeral, content: `Linked discord user ${userMention(interaction.targetUser.id)} to SLMN.GG user ${slmnggUser}` });
+                        })
+                        .catch(({
+                            errorCode,
+                            errorMessage
+                        }) => {
+                            console.error({ errorCode, errorMessage });
+                            interaction.followUp({
+                                ephemeral, content: `Action failed: ${errorMessage}`
+                            });
+                        });
+                }
+            }
+
+        });
     }
 };

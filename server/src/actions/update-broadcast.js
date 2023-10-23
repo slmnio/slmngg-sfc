ca24bdfe4410526802e08d961d5f4df5ba9ca0bd
Solomon Cammack <solomon@solomon.eu.com>
2023-10-23 08:48:27 +0100

    Add countdown timer buttons to dashboard

--- server/src/actions/update-broadcast.js
+++ server/src/actions/update-broadcast.js
@@ -3,14 +3,14 @@ const { safeInput, safeInputNoQuotes } = require("../action-utils/action-utils")
 module.exports = {
     key: "update-broadcast",
     auth: ["client"],
-    optionalParams: ["match", "advertise", "playerCams", "mapAttack", "title", "manualGuests", "deskDisplayMode", "deskDisplayText", "showLiveMatch"],
+    optionalParams: ["match", "advertise", "playerCams", "mapAttack", "title", "manualGuests", "deskDisplayMode", "deskDisplayText", "showLiveMatch", "countdownEnd"],
     /***
      * @param {AnyAirtableID} match
      * @param {ClientData} client
      * @returns {Promise<void>}
      */
     // eslint-disable-next-line no-empty-pattern
-    async handler({ match: matchID, advertise, playerCams, mapAttack, title, manualGuests, deskDisplayMode, deskDisplayText, showLiveMatch }, { client }) {
+    async handler({ match: matchID, advertise, playerCams, mapAttack, title, manualGuests, deskDisplayMode, deskDisplayText, showLiveMatch, countdownEnd }, { client }) {
         let broadcast = await this.helpers.get(client?.broadcast?.[0]);
         if (!broadcast) throw ("No broadcast associated");
 
@@ -53,6 +53,9 @@ module.exports = {
         }
         if (deskDisplayText !== undefined) {
             validatedData["Notice Text"] = safeInputNoQuotes(deskDisplayText);
+        }
+        if (countdownEnd !== undefined) {
+            validatedData["Countdown End"] = countdownEnd ? new Date(countdownEnd) : countdownEnd;
         }
 
         console.log(validatedData);

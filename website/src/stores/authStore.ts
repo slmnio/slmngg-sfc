fb0340d17b24ef8497a929a3a43ad29d997fea42
Solomon Cammack <solomon@solomon.eu.com>
2024-04-24 06:27:25 +0100

    Add lookups for authed user, only persist token

--- website/src/stores/authStore.ts
+++ website/src/stores/authStore.ts
@@ -1,20 +1,73 @@
 import {defineStore} from "pinia";
 import {computed, ref} from "vue";
 import {getDataServerAddress, getMainDomain} from "@/utils/fetch";
+import {ReactiveRoot} from "@/utils/reactive";
+
+type Snowflake = string;
+type DiscordID = Snowflake;
+type CleanAirtableID = string;
+type DirtyAirtableID = `rec${CleanAirtableID}`;
+type AnyAirtableID = CleanAirtableID | DirtyAirtableID;
+
+type WebUserData = {
+    discordID: DiscordID;
+    airtableID: AnyAirtableID;
+    name: string;
+    avatar: string;
+    website_settings: string[];
+}
+
+type Player = {
+    id: DirtyAirtableID;
+    name: string;
+    pronunciation?: string;
+    pronouns?: string;
+    role?: string;
+    verified?: boolean;
+    twitter_handle?: string[];
+    twitter_link?: string[];
+    website_settings?: string[];
+    battletag?: string;
+    discord_tag?: string;
+    discord_id?: DiscordID;
+    member_of?: DirtyAirtableID[];
+    event_staff?: DirtyAirtableID[];
+    team_staff?: DirtyAirtableID[];
+    casts?: DirtyAirtableID[];
+    casted_events?: DirtyAirtableID[];
+    socials?: DirtyAirtableID[];
+    news?: DirtyAirtableID[];
+    live_guests?: DirtyAirtableID[];
+    player_relationships?: DirtyAirtableID[];
+    clients?: DirtyAirtableID[];
+    teams?: DirtyAirtableID[];
+    favourite_hero?: DirtyAirtableID[];
+    highlighted_on?: DirtyAirtableID[];
+}
+type Client = {
+    name: string;
+    key: string;
+    staff: DirtyAirtableID[];
+    broadcast: DirtyAirtableID[];
+    id: DirtyAirtableID;
+}
 
 export const useAuthStore = defineStore("auth", () => {
 
     const token = ref<string | null>(null);
     const authNext = ref<string | null>(null);
-    const user = ref<any | null>(null);
+    const user = ref<WebUserData | null>(null);
     const auth = computed(() => ({
         token: token.value,
         user: user.value
     }))
 
+    const player = computed<Player>(() => user.value ? ReactiveRoot(user.value.airtableID) : null)
+    const client = computed<Client>(() => player.value?.id ? ReactiveRoot(player.value.clients?.[0]) : null)
+
     const isAuthenticated = computed(() => !!user.value);
     const isProduction = computed(() => {
-        return !!user.value?.clients?.length
+        return !!player.value?.clients?.length
     })
 
     async function authenticateWithDiscord(code: string) {
@@ -102,6 +155,9 @@ export const useAuthStore = defineStore("auth", () => {
         token,
         user,
         auth,
+        player,
+        client,
+
         isAuthenticated,
         isProduction,
         authenticateWithDiscord,
@@ -111,4 +167,8 @@ export const useAuthStore = defineStore("auth", () => {
     }
 
 
-}, {persist: true});
+}, {
+    persist: {
+        paths: ["token"]
+    }
+});

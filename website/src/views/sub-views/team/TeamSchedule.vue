d6a833ffd0612ebb19d6684ace21b678d0658778
Tobias Messner <tobias@zusor.io>
2024-06-20 07:00:19 +0200

    Expand iCal to include teams & add public UI (#197)
    * Add team support to ical endpoint, start work on interface for importing ical calendar
    
    * Add buttons to event & team schedules, integrate with vue3 changes
    
    ---------
    
    Co-authored-by: Solomon Cammack <solomon@solomon.eu.com>

--- website/src/views/sub-views/team/TeamSchedule.vue
+++ website/src/views/sub-views/team/TeamSchedule.vue
@@ -1,6 +1,23 @@
 <template>
     <div class="container">
-        <h2 class="text-center mb-3">Team matches</h2>
+        <div class="schedule-title">
+            <h2 class="text-center">Schedule</h2>
+
+            <div class="d-flex w-100 flex-column align-items-end gap-1 top-right-settings">
+                <b-dropdown auto-close="outside">
+                    <template #button-content>
+                        <i class="fas fa-cog fa-fw mr-1"></i>
+                        <span class="d-none d-lg-inline-block" style="line-height:1">Settings & Sync</span>
+                    </template>
+                    <div class="dropdown-content d-flex flex-column align-items-end gap-3 p-3" style="min-width: min(100vw, 300px)">
+                        <TimezoneSwapper align="left" :inline="true" />
+                        <AddToCalendar :team="team" />
+                    </div>
+                </b-dropdown>
+            </div>
+        </div>
+
+
         <div class="w-100">
             <ScheduleMatch
                 v-for="match in matches"
@@ -18,10 +35,14 @@ import { ReactiveArray, ReactiveRoot, ReactiveThing } from "@/utils/reactive";
 import { sortMatches } from "@/utils/sorts";
 import { canEditMatch } from "@/utils/client-action-permissions";
 import { useAuthStore } from "@/stores/authStore";
+import TimezoneSwapper from "@/components/website/schedule/TimezoneSwapper.vue";
+import AddToCalendar from "@/components/website/AddToCalendar.vue";
 
 export default {
     name: "TeamMatches",
     components: {
+        AddToCalendar,
+        TimezoneSwapper,
         ScheduleMatch
     },
     props: ["team"],
@@ -50,5 +71,15 @@ export default {
 </script>
 
 <style scoped>
-
+    .top-right-settings {
+        position: absolute;
+        top: 0;
+        right: 0;
+    }
+    .schedule-title {
+        position: relative;
+    }
+    .dropdown-content {
+        margin: -.5em 0;
+    }
 </style>

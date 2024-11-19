3fa446b0b09a15ea9735452009f723d399d0a6b7
MrLotU <j.koopman@jarict.nl>
2024-11-19 16:29:40 +0100

    wip

--- website/src/components/broadcast/roots/RunOfShow.vue
+++ website/src/components/broadcast/roots/RunOfShow.vue
@@ -1,25 +1,27 @@
 <template>
     <div class="rosHolder">
         <div class="rosControls">
-            <button :disabled="this.rosItemIdx === 0">Prev</button>
+            <button :disabled="this.rosItemIdx === 0" @click="prev()">Prev</button>
+            <button @click="this.reset()">Reset</button>
             <button @click="toggleRunning()">{{ running ? 'Pause' : 'Resume' }}</button>
             <button :disabled="this.rosItemIdx + 1 === this.rosItems.length" @click="next()">Next</button>
         </div>
         <div class="rosSegments">
             <div class="currentSegment">
-                <div class="currentInfo"></div>
+                <div class="currentInfo">A</div>
                 <div class="currentTimelineHolder">
                     <div class="currentTimeline">
-                        <div>{{ getTimeElapsed(counter) }}</div>
-                        <div>{{ getTimeElapsed(counter, true) }}</div>
+                        <div class="timer">{{ getTimeElapsed(counter) }}</div>
+                        <div class="flex-initial" style="width: 100%;"></div>
+                        <div class="timer">{{ getTimeElapsed(counter, true) }}</div>
                     </div>
                     <div class="timelineProgress" :style="{ width: getTimelineSegmentWidth(counter) }"></div>
                     <!-- {{ getTimeElapsed(counter) }} | {{ getTimeElapsed(counter, true) }} {{ counter }} -->
                 </div>
             </div>
             <div class="nextSegment">
-                <div class="nextInfo"></div>
-                <div class="nextTimeline"></div>
+                <div class="nextInfo">B</div>
+                <div class="nextTimeline">B</div>
             </div>
         </div>
     </div>
@@ -93,6 +95,13 @@ export default {
             this.reset();
             if (!this.running) this.resume();
         },
+        prev() {
+            if (this.rosItemIdx === 0) return this.pause();
+            this.rosItemIdx -= 1;
+            if (!this.counter) return this.start();
+            this.reset();
+            if (!this.running) this.resume();
+        },
         toggleRunning() {
             if (!this.counter) return this.start();
             this.running ? this.pause() : this.resume();
@@ -102,9 +111,13 @@ export default {
             console.log(count);
         },
         getTimeElapsed(c, inv=false) {
-            if (!c) return 0;
+            console.log(c);
+            if (!c) c = 0;
             if (c.value) c = c.value;
+            if (isNaN(c)) c = 0;
             if (inv) c = this.ticksRequired - c;
+            console.log(c);
+            if (c === 0) return "00:00:00:00";
             const pad = (n) => {
                 return ("0" + n).slice(-2);
             };
@@ -113,7 +126,7 @@ export default {
             const hours = pad(Math.floor(minutes / 60));
             const realSeconds = pad(seconds % 60);
             const realMinutes = pad(minutes % 60);
-            const ms = c % 100;
+            const ms = pad(c % 100);
 
             return `${hours}:${realMinutes}:${realSeconds}:${ms}`;
         },
@@ -142,15 +155,24 @@ export default {
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin: 2em;
+    height: 100vh;
+    width: 100vw;
+    background-color: #000000;
+    color: #ffffff;
+    place-items: center;
+    font-size: clamp(20px, 3vw, 25vh);
+    font-family: "SLMN-Industry", "Industry", sans-serif;
+    text-align: center;
 }
 .rosSegments {
     display: flex;
+    flex-direction: row;
     width: 100%;
     align-items: center;
 }
 .currentSegment {
     display: flex;
+    flex-direction: column;
     width: 100%;
     align-items: center;
     justify-content: center;
@@ -163,20 +185,27 @@ export default {
     position: relative;
     width: 85%;
     background-color: red;
+    border-radius: 6px;
+    overflow: hidden;
 }
 .currentTimeline {
     position: relative;
     display: flex;
     z-index: 1;
     flex-direction: row;
-    column-gap: 1px;
+    padding: 0px 6px;
     width: 100%;
 }
 .timelineProgress {
     background-color: green;
     position: absolute;
     z-index: 0;
-    height: 24px;
+    height: 100%;
     top: 0px;
+    border-radius: 6px;
+}
+.timer {
+    text-align: left;
+    min-width: 335px;
 }
 </style>

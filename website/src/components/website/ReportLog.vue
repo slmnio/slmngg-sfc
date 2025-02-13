48e4237ec54f77b3fc641e16c53a6519b1ea8ab6
Solomon Cammack <solomon@solomon.eu.com>
2025-02-13 07:29:48 +0000

    Update logs to show previous requests and icons

--- website/src/components/website/ReportLog.vue
+++ website/src/components/website/ReportLog.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="report-log">
         <div v-for="step in steps" :key="step.key" class="step">
-            <span v-if="step.time">{{ step.time }}:&nbsp;</span>
+            <span v-if="step.time" class="time">{{ step.time }}:&nbsp;</span>
+            <span v-if="stepIcon[step.key]"><i :class="`fa-fw ${stepIcon[step.key]}`"></i>&nbsp;</span>
             <span v-if="step.user"><router-link :to="url('player', step.user)">{{ step.user?.name }}</router-link>&nbsp;</span>
             <span v-if="step.text" class="action">{{ step.text.slice(0,1).toLowerCase() + step.text.slice(1) }}&nbsp;</span>
             <span v-if="step.team">as <router-link :to="url('team', step.team)">{{ step.team?.name }}</router-link>&nbsp;</span>
+            <span v-if="step.key === 'submitted_request' && existingReportData?.start" class="submission">for {{ formatTime(existingReportData?.start) }}</span>
             <span v-else-if="step.staff">as staff&nbsp;</span>
         </div>
     </div>
@@ -17,7 +19,8 @@ import { formatTime, url } from "@/utils/content-utils.js";
 export default {
     name: "ReportLog",
     props: {
-        log: String
+        log: String,
+        report: Object
     },
     computed: {
         steps() {
@@ -30,11 +33,45 @@ export default {
 
                 return step;
             });
+        },
+        existingReportData() {
+            if (!this.report?.data) return null;
+            try {
+                return JSON.parse(this.report.data);
+            } catch (e) {
+                console.error(e);
+                return null;
+            }
+        },
+        stepIcon() {
+            return {
+                "approved_by_opponent": "fas fa-check",
+                "staff_preapproved": "fas fa-check",
+                "staff_approved": "fas fa-check",
+                "staff_force_approved": "fas fa-shield-check",
+                "staff_force_approved_counter": "fas fa-shield-check",
+                "approved_counter_report": "fas fa-check",
+
+                "deleted": "fas fa-trash",
+                "staff_deleted": "fas fa-trash",
+
+                "denied_by_opponent": "fas fa-times",
+                "staff_denied": "fas fa-times-octagon",
+
+                "submitted_request": "fas fa-check",
+                "submitted_score_report": "fas fa-check",
+                "countered_score_report": "fas fa-exchange",
+            };
         }
     },
-    methods: { url }
+    methods: { formatTime, url }
 };
 </script>
 
 <style scoped>
+.time {
+    min-width: 15ex;
+    display: inline-block;
+    text-align: right;
+}
 </style>

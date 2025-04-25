3a973b1336e0b2eed0316696f0803b408b4a9d2d
MrLotU <j.koopman@jarict.nl>
2025-04-25 18:40:36 -0400

    Add rivals settings to games.ts

--- website/src/utils/games.ts
+++ website/src/utils/games.ts
@@ -1,4 +1,4 @@
-export const Games = ["Overwatch", "Deadlock", "League of Legends", "Valorant"];
+export const Games = ["Overwatch", "Deadlock", "League of Legends", "Valorant", "Marvel Rivals"];
 
 export const GameOverrides = {
     "League of Legends": {
@@ -19,6 +19,23 @@ export const GameOverrides = {
             "ban2,ban1,ban2,ban1," +
             "pick2,pick1,pick1,pick2"
     },
+    "Marvel Rivals": {
+        showForProduction: [
+            "replay_code",
+            "public"
+        ],
+        defaultPickBanOrder: "ban2,ban1,pick1,pick2,ban2,ban1,pick1,pick2,ban2,ban1",
+        defaultHeroBanCount: 3,
+        defaultHeroPickCount: 2,
+        lang: {
+            "replay_code": "Match UUID"
+        },
+        formStyles: {
+            "replay_code": {
+                "min-width": "6em"
+            }
+        }
+    },
     "Valorant": {
         useBestOf: true,
         lang: {
@@ -31,7 +48,7 @@ export const GameOverrides = {
         disableMapPicks: true,
         disableMapBans: true,
         disableMapScore: true,
-        defaultHeroBanCount: 1,
+        defaultHeroBanCount: 2,
         defaultHeroPickCount: 6,
         showDraftControlButtons: true,
         lang: {

b01b0583f4f4aec45323e1ade2f0379ad79ccdb7
Solomon Cammack <solomon@solomon.eu.com>
2025-06-26 01:27:33 +0100

    Revert "Update base URL by adding VITE_BASE_URL to avoid assets being served by subdomains. Closes #247"
    This reverts commit 5bfde5f5e22ec157b7817e1e2fa48e496d23ffef.

--- website/vite.config.mjs
+++ website/vite.config.mjs
@@ -1,7 +1,7 @@
 import createVuePlugin from "@vitejs/plugin-vue";
 import dns from "dns";
 import { fileURLToPath, URL } from "url";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import Components from "unplugin-vue-components/vite";
 import { BootstrapVueNextResolver } from "unplugin-vue-components/resolvers";
 
@@ -9,32 +9,28 @@ import { BootstrapVueNextResolver } from "unplugin-vue-components/resolvers";
 dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '')
-    return {
-        plugins: [
-            createVuePlugin(),
-            Components({
-                resolvers: [BootstrapVueNextResolver()]
-            })
-        ],
-        base: env?.VITE_BASE_URL || "/",
-        build: {
-            assetsInlineLimit: 0,
+export default defineConfig(({ mode }) => ({
+    plugins: [
+        createVuePlugin(),
+        Components({
+            resolvers: [BootstrapVueNextResolver()]
+        })
+    ],
+    build: {
+        assetsInlineLimit: 0
+    },
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
-        resolve: {
-            alias: {
-                "@": fileURLToPath(new URL("./src", import.meta.url)),
-            },
-            extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"]
-        },
-        define: {
-            __VUE_PROD_DEVTOOLS__: ["local", "staging"].includes(mode)
-        },
-        esbuild: {
-            supported: {
-                "top-level-await": true
-            }
+        extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"]
+    },
+    define: {
+        __VUE_PROD_DEVTOOLS__: ["local", "staging"].includes(mode)
+    },
+    esbuild: {
+        supported: {
+            "top-level-await": true
         }
-    };
-});
+    }
+}));

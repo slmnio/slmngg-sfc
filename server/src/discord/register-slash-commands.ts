d10b294294ab207013c1f8d47eaed7ca2a344159
Solomon Cammack <solomon@solomon.eu.com>
2025-12-08 22:35:16 +0000

    Update register-slash-commands

--- server/src/discord/register-slash-commands.ts
+++ server/src/discord/register-slash-commands.ts
@@ -1,13 +1,18 @@
 import "dotenv/config";
-import { REST, Routes } from "discord.js";
+import { REST, Routes, SlashCommandBuilder } from "discord.js";
 import fs from "node:fs";
 import path from "node:path";
 
 import { fileURLToPath, pathToFileURL } from "node:url";
 
 const DIRNAME = path.dirname(fileURLToPath(import.meta.url));
+if (!(process.env.DISCORD_TOKEN) || !(process.env.DISCORD_CLIENT_ID)) throw new Error("No discord token in .env");
 
-const commands = [];
+interface SLMNggDiscordCommand {
+    data: SlashCommandBuilder
+}
+
+const commands: SLMNggDiscordCommand[] = [];
 // Grab all the command files from the commands directory you created earlier
 const foldersPath = path.join(DIRNAME, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
@@ -18,7 +23,7 @@ for (const folder of commandFolders) {
     const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js") || file.endsWith(".ts"));
     // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
     for (const file of commandFiles) {
-        const filePath = pathToFileURL(path.join(commandsPath, file));
+        const filePath = pathToFileURL(path.join(commandsPath, file)).toString();
         const command = (await import(filePath))?.default;
         if ("data" in command && "execute" in command) {
             commands.push(command.data.toJSON());
@@ -33,14 +38,15 @@ const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 
 // and deploy your commands!
 const registerCommands = (async () => {
+    if (!(process.env.DISCORD_TOKEN) || !(process.env.DISCORD_CLIENT_ID)) throw new Error("No discord token in .env");
     try {
         console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
         // The put method is used to fully refresh all commands in the guild with the current set
         const data = await rest.put(
             Routes.applicationCommands(process.env.DISCORD_CLIENT_ID),
-            { body: commands },
-        );
+            {body: commands},
+        ) as any[];
 
         console.log(`Successfully reloaded ${data.length} application (/) commands.`);
     } catch (error) {

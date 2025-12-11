c7e61fc6ff234c02604bd945aba76cdfaa78a7c0
Solomon Cammack <solomon@solomon.eu.com>
2025-12-11 07:37:04 +0000

    Fix issue with dirtyID and types that aren't known

--- shared/index.ts
+++ shared/index.ts
@@ -11,13 +11,13 @@ export function cleanID(id: AnyAirtableID | null | undefined | { id: AnyAirtable
 }
 
 export function cleanTypedID<IDType extends string>(id: (IDType | DirtyAirtableID)): IDType {
-    if (id.startsWith("rec") && id.length === 17) return id.slice(3) as IDType;
+    if (id?.startsWith("rec") && id.length === 17) return id.slice(3) as IDType;
     return id as IDType;
 }
 
 // Allow a passthrough type for now - almost everything will be a dirty ID and the distinction to clean needs to be
 export function dirtyID<IDType extends string>(id: IDType) {
-    if (id.length === 14) return "rec" + id as IDType;
+    if (id?.length === 14) return "rec" + id as IDType;
     return id as IDType;
 }
 
